caf1dba23314c2504473f081d297704de6921468
Shubham Rajak <shubham@neelnetworks.com>
2025-07-16 18:50:44 +0530

    fix: Change initial user state from null to false in UserProvider

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useContext, useState } from 'react'
 export const UserContext  = createContext();
 
 export const UserProvider = ({children}) =>{
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(false);
     
     return (
         <UserContext.Provider value={{ user, setUser }}>
